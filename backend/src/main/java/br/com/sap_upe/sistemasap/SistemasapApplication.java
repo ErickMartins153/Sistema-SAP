a11bcae5d1dd11d98769aff9d1073ffc6c842737
LuanVN4 <luanvilaca1@gmail.com>
2024-01-17 15:14:40 -0300

    chore: adicionando repositório backend

---
+++ backend/src/main/java/br/com/sap_upe/sistemasap/SistemasapApplication.java
@@ -0,0 +1,13 @@
+package br.com.sap_upe.sistemasap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/sap_upe/sistemasap/SistemasapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sap_upe.sistemasap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
