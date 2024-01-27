0d9cbba656b6ba54222b885a0630e0b2f5ecafde
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-01-27 18:47:04 -0300

    feat: allow deletion of users

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,33 +1,79 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 import { Colors, GlobalStyles } from "../constants/style";
+import UserAvatar from "./UI/UserAvatar";
+import { useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 export default function User({ userData }) {
+  const { deleteUser } = useContext(AuthContext);
+
+  function handlePress() {
+    Alert.alert(
+      "Você tem certeza?",
+      "Uma vez deletado esse usuário não poderá logar!",
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Confirmar",
+          style: "destructive",
+          onPress: handleDeleteUser,
+        },
+      ]
+    );
+  }
+
+  function handleDeleteUser() {
+    deleteUser(userData.name);
+  }
+
   return (
-    <View style={styles.rootContainer}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>{userData.name}</Text>
+    <Pressable
+      style={({ pressed }) => [styles.rootContainer, pressed && styles.pressed]}
+      onPress={handlePress}
+      android_ripple={{ color: Colors.accentColor }}
+    >
+      <View style={styles.avatar}>
+        <UserAvatar size={64} />
       </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.text}>{userData.email}</Text>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>{userData.name}</Text>
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{userData.email}</Text>
+        </View>
       </View>
-    </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
-    backgroundColor: Colors.white,
+    backgroundColor: Colors.buttonRipple,
     justifyContent: "center",
-    borderWidth: 1,
     borderRadius: 10,
     marginVertical: 16,
     marginHorizontal: 32,
+    paddingVertical: 2,
+    paddingHorizontal: 12,
+    overflow: "hidden",
+    flexDirection: "row",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+  content: {
+    flex: 1,
+    borderRadius: 10,
     overflow: "hidden",
   },
   header: {
     flex: 1,
     padding: 4,
     backgroundColor: Colors.accentColor,
+  },
+  avatar: {
+    width: 69,
   },
   headerText: {
     ...GlobalStyles.smallText,
